9bc4b09712db0c1daeaa1a7afcef8174727b803a
Erika-Andrade <erikaandradesuz@gmail.com>
2024-09-18 18:44:01 -0500

    insert cuentas completo, insert productos en proceso

--- scripts/creacion_cuentas.sql
+++ scripts/creacion_cuentas.sql
@@ -4,5 +4,21 @@ create table cuentas(
 	fecha_creacion date not null,
 	saldo money not null,
 	constraint cuentas_pk primary key(numero_cuenta)
-	
 )
+insert into cuentas(numero_cuenta,cedula_propietario,fecha_creacion,saldo)
+values('23338','10239','19/02/2008',2008.50)
+
+insert into cuentas(numero_cuenta,cedula_propietario,fecha_creacion,saldo)
+values('23338','10239','19/02/2008',2008.50)
+
+insert into cuentas(numero_cuenta,cedula_propietario,fecha_creacion,saldo)
+values('23338','10239','19/02/2008',2008.50)
+
+insert into cuentas(numero_cuenta,cedula_propietario,fecha_creacion,saldo)
+values('23338','10239','19/02/2008',2008.50)
+
+insert into cuentas(numero_cuenta,cedula_propietario,fecha_creacion,saldo)
+values('23338','10239','19/02/2008',2008.50)
+
+insert into cuentas(numero_cuenta,cedula_propietario,fecha_creacion,saldo)
+values('23338','10239','19/02/2008',2008.50)
--- scripts/creacion_productos.sql
+++ scripts/creacion_productos.sql
@@ -7,3 +7,30 @@ create table productos(
 	constraint productos_pk primary key(codigo)
 	
 )
+insert into productos(codigo,nombre,descripcion,precio,stock)
+values('001','Coca Cola','Lite',1.50,12)
+
+insert into productos(codigo,nombre,descripcion,precio,stock)
+values('034','Agua mineral','Agua purificada y embotellada',0.65,20)
+
+insert into productos(codigo,nombre,descripcion,precio,stock)
+values('778','Frutaris','Jugo sabor naranja',0.75,30)
+
+insert into productos(codigo,nombre,descripcion,precio,stock)
+values('900','Azucar','Azucar refinada',2.30,89)
+
+insert into productos(codigo,nombre,descripcion,precio,stock)
+values('899','Yogurt Ta´rico','2L Sabor Mora y Fresa',7.60,8)
+
+insert into productos(codigo,nombre,precio)
+values('898','Leche Toni',0.95)
+
+insert into productos(codigo,nombre,precio)
+values('766','Papas Ruffles',2.00)
+
+insert into productos(codigo,nombre,precio)
+values('99','Polito',0.25)
+
+
+select * from productos
+
