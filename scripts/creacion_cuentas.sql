0d0f44f3e2242fdb2e09f99ab7082bcdead93e11
Erika-Andrade <erikaandradesuz@gmail.com>
2024-09-19 11:37:02 -0500

    insert en tablas cuentas y estudiantes completo

--- scripts/creacion_cuentas.sql
+++ scripts/creacion_cuentas.sql
@@ -5,20 +5,36 @@ create table cuentas(
 	saldo money not null,
 	constraint cuentas_pk primary key(numero_cuenta)
 )
+--1
 insert into cuentas(numero_cuenta,cedula_propietario,fecha_creacion,saldo)
 values('23338','10239','19/02/2008',2008.50)
+--2
+insert into cuentas(numero_cuenta,cedula_propietario,fecha_creacion,saldo)
+values('33333','09345','08/03/2023',708.75)
+--3
+insert into cuentas(numero_cuenta,cedula_propietario,fecha_creacion,saldo)
+values('78999','73449','24/11/1999',10344.98)
+--4
+insert into cuentas(numero_cuenta,cedula_propietario,fecha_creacion,saldo)
+values('90099','10021','19/09/2024',3000)
+--5
+insert into cuentas(numero_cuenta,cedula_propietario,fecha_creacion,saldo)
+values('93535','43373','9/01/2021',2008.50)
+--6
+insert into cuentas(numero_cuenta,cedula_propietario,fecha_creacion,saldo)
+values('16055','97117','14/11/2004',929.09)
+--7
+insert into cuentas(numero_cuenta,cedula_propietario,fecha_creacion,saldo)
+values('43511','89937','28/12/2017',8114.57)
+--8
+insert into cuentas(numero_cuenta,cedula_propietario,fecha_creacion,saldo)
+values('52456','77056','30/08/2018',92.10)
+--9
+insert into cuentas(numero_cuenta,cedula_propietario,fecha_creacion,saldo)
+values('47720','14655','10/10/2015',5645.23)
+--10
+insert into cuentas(numero_cuenta,cedula_propietario,fecha_creacion,saldo)
+values('63005','14886','12/02/2000',680.07)
 
-insert into cuentas(numero_cuenta,cedula_propietario,fecha_creacion,saldo)
-values('23338','10239','19/02/2008',2008.50)
 
-insert into cuentas(numero_cuenta,cedula_propietario,fecha_creacion,saldo)
-values('23338','10239','19/02/2008',2008.50)
-
-insert into cuentas(numero_cuenta,cedula_propietario,fecha_creacion,saldo)
-values('23338','10239','19/02/2008',2008.50)
-
-insert into cuentas(numero_cuenta,cedula_propietario,fecha_creacion,saldo)
-values('23338','10239','19/02/2008',2008.50)
-
-insert into cuentas(numero_cuenta,cedula_propietario,fecha_creacion,saldo)
-values('23338','10239','19/02/2008',2008.50)
+select * from cuentas
--- scripts/creacion_estudiantes.sql
+++ scripts/creacion_estudiantes.sql
@@ -6,3 +6,32 @@ create table estudiantes(
 	fecha_nacimiento date not null,
 	constraint estudiantes_pk primary key(cedula)
 )
+--1
+insert into estudiantes(cedula,nombre,apellido,email,fecha_nacimiento)
+values('1001277333','Hannibal','Lecter','hanni609L@gmail.com','17/10/1999')
+--2
+insert into estudiantes(cedula,nombre,apellido,email,fecha_nacimiento)
+values('1101160032','Alicia','Jimenez','idkidkL@hotmail.com','08/1/2002')
+
+--3
+insert into estudiantes(cedula,nombre,apellido,email,fecha_nacimiento)
+values('0112262499','Alexandra','Zurita','zuriAlexa@gmail.com','24/03/2010')
+
+--4
+insert into estudiantes(cedula,nombre,apellido,email,fecha_nacimiento)
+values('1717795994','Antonio','Perarla','toniPerr444@gmail.com','21/7/2008')
+
+--5
+insert into estudiantes(cedula,nombre,apellido,email,fecha_nacimiento)
+values('6033046848','Nora','Condor','alexita555dr@gmail.com','09/12/2001')
+--6
+insert into estudiantes(cedula,nombre,apellido,email,fecha_nacimiento)
+values('0921401378','Giselle','Aguilar','ikikik78@gmail.com','05/11/2006')
+--7
+insert into estudiantes(cedula,nombre,apellido,email,fecha_nacimiento)
+values('1304734625','Victor','Ramos','6777yuhuuj@yahoo.com','03/5/2009')
+--8
+insert into estudiantes(cedula,nombre,apellido,email,fecha_nacimiento)
+values('1003445988','Byron','Ayala','estabanguacan10@gmail.com','23/4/2005')
+
+select * from estudiantes
